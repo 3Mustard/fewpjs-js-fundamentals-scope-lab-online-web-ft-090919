f22ea4d9547b58010dccaff2d54d9e07ff3f6bc8
Matthew C <m23.cullen@gmail.com>
2019-12-17 23:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const animal = "dog";
 
 function myAnimal() {
   return animal
-};
+}
 
 function yourAnimal() {
   const animal = "cat";
   return animal
-};
+}
 
 function add2(n) {
   const two = 2;
   return n +two
-};
+}
